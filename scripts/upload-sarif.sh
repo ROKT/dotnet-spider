a82995ac58a90cadc84920ef2cd316438c499d93
Ashwin Nirantar <ashwin.nirantar@rokt.com>
2021-07-08 11:09:11 -0400

    dummy file

--- scripts/upload-sarif.sh
+++ scripts/upload-sarif.sh
@@ -6,8 +6,7 @@ echo "Switching PWD to scripts"
 cd scripts
 sarif=$(gzip -c scs-sarif.sh | base64)
 
-curl \ 
-    -X POST \
+curl -X POST \
     -H "Accept: application/vnd.github.v3+json" \
-    https://api.github.com/repos/ROKT/campaignsplatform/code-scanning/sarifs \
-    -d '{"commit_sha":"9489c1a4f61a90dfc1f7fdd3c77df805840d9ff7", "ref":"refs/remotes/origin/feature/scs-sarif","sarif":"${sarif}"}'
+    "https://api.github.com/repos/ROKT/dotnet-spider/code-scanning/sarifs" \
+    -d '{"commit_sha":"d503cdc478da56bce6febacc1f0444e6b2c6827a","ref":"refs/remotes/origin/feature/scs-sarif","sarif":"${sarif}"}'
