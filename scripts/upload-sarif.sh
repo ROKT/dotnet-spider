be6494e1433d5b8b4f85e7ffc75dbe2edb3e9439
Ashwin Nirantar <ashwin.nirantar@rokt.com>
2021-07-10 13:27:55 -0700

    removed access token from script

--- scripts/upload-sarif.sh
+++ scripts/upload-sarif.sh
@@ -9,7 +9,7 @@ echo "Authenticating user using personal access token"
 
 echo "Uploading SARIF to Github"
 curl -X POST \
-    -v -H "Authorization: token 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" \
+    -v -H "Authorization: token " \
     -H "Accept: application/vnd.github.v3+json" \
     "https://api.github.com/repos/ROKT/dotnet-spider/code-scanning/sarifs" \
     -d '{"commit_sha":"${commit_sha}","ref":"refs/heads/feature/scs-sarif","sarif":"${sarif}"}'
