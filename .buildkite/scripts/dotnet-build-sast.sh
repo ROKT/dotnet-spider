5bdc403af6724cd81cac8a867bca854fc5e270c9
Ashwin Nirantar <ashwin.nirantar@rokt.com>
2021-12-09 15:41:10 -0500

    sonarqube for dotnet-spider

---
+++ .buildkite/scripts/dotnet-build-sast.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Restores and builds the solution for SAST"
+
+SONARQUBE_HOST_URL="https://sonarqube.eng.roktinternal.com"
+PROJECT_NAME="dotnet-spider"
+SONARQUBE_TOKEN_VALUE_ENV=${1:?"Missing argument \$1 for SonarQube token"}
+
+dotnet restore DotnetSpider.sln \
+    --verbosity quiet
+
+find ./src -name '*.csproj' | xargs -I % dotnet add % package \
+    Microsoft.Net.Compilers.Toolset --version 3.8.0
+
+find ./src -name '*.csproj' | xargs -I % dotnet add % package \
+    SecurityCodeScan.VS2019 --version 5.1.0
+
+dotnet sonarscanner begin \
+        /k:"${PROJECT_NAME}" \
+        /d:sonar.host.url="${SONARQUBE_HOST_URL}" \
+        /d:sonar.login="${SONARQUBE_TOKEN_VALUE_ENV}" \
+        /d:sonar.branch.name="feature/sonarqube"
+
+find ./src -name '*.csproj' | xargs -I % dotnet build % >> scs_results.txt
+
+dotnet sonarscanner end \
+        /d:sonar.login="${SONARQUBE_TOKEN_VALUE_ENV}"
